e231d8da546456e67689a6dab4e27ed6e0ac6e7c
rir360 <rezowanislamrizvy7@gmail.com>
2023-11-20 02:33:35 +0600

    basic backend configuration

--- txt-thread.js
+++ txt-thread.js
@@ -1,12 +1,21 @@
+require("dotenv").config();
 const express = require('express');
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3300;
 
-app.get('/', (req, res) => {
-  res.send('Hello, TXT Thread!');
-});
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.static(path.join(__dirname + '/public')));
+
+const index_router = require("./routes/index");
+
+app.use("/", index_router);
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+
+  console.log(`Server listening on port http://localhost:${port}`);
+  
 });
