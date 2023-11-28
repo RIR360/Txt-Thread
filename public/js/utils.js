42759ccc2c3e1d0b12bee00e42841899dd0f4a10
rir360 <rezowanislamrizvy7@gmail.com>
2023-11-28 18:01:38 +0600

    functional loader in between redirect

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,26 +1,42 @@
 async function redirect(url) {
 
-  return $.ajax({
-    url,
-    method: "GET",
-    success: (data) => {
+  try {
 
-      if (data.status === "error") {
+    if (!url && url === "") return;
 
-        console.log(data);
+    // set loading state
+    $main_container.html(TEMPLATE.loader);
 
-      } else {
+    await $.ajax({
+      url,
+      method: "GET",
+      success: (data) => {
 
-        $main_container.html(data);
+        if (data.status === "error") {
+  
+          console.log(data);
+          redirect("/error");
+
+        } else {
+
+          $main_container.html(data);
+
+        }
+
+      },
+      error: (xhr, status, err) => {
+
+        $main_container.html(TEMPLATE.error);
+        console.error(status);
 
       }
+    })
+    
+  } catch (err) {
 
-    },
-    error: (xhr, status, err) => {
-
-      console.error(status);
-
-    }
-  })
+    $main_container.html(TEMPLATE.error);
+    console.error(err);
+    
+  }
   
 }
